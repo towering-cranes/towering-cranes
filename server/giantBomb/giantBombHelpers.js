e86da244ec1c4b7ae4d7bc30716a0521742aa429
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-02-10 12:57:01 -0600

    Update giantBombHelpers.js

--- server/giantBomb/giantBombHelpers.js
+++ server/giantBomb/giantBombHelpers.js
@@ -1,7 +1,6 @@
 var request = require('request');
 
-//var giantBombApiKey = process.env.GIANTBOMB_API_KEY;
-var giantBombApiKey = '138240b063cac215ab7ed7413514c1456d3fd46d';
+var giantBombApiKey = process.env.GIANTBOMB_API_KEY;
 
 exports.searchForGames = function(searchTerm, callback) {
   var options = {
