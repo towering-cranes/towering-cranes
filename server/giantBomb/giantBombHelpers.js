e04102cd7d6e3f3318d8234d538718623a56f9f6
gwynndp <gwynndp@gmail.com>
2017-02-09 20:07:38 -0800

    Insert GiantBomb API key

--- server/giantBomb/giantBombHelpers.js
+++ server/giantBomb/giantBombHelpers.js
@@ -1,6 +1,7 @@
 var request = require('request');
 
-var giantBombApiKey = process.env.GIANTBOMB_API_KEY;
+//var giantBombApiKey = process.env.GIANTBOMB_API_KEY;
+var giantBombApiKey = '138240b063cac215ab7ed7413514c1456d3fd46d';
 
 exports.searchForGames = function(searchTerm, callback) {
   var options = {
