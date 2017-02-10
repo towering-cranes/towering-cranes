09a66e2fc36b7ede7dc38cb24b47ef78bc9527e8
gwynndp <gwynndp@gmail.com>
2017-02-09 19:50:23 -0800

    Update to use ClearDB url

--- server/database/db.js
+++ server/database/db.js
@@ -3,6 +3,12 @@
 
 var Sequelize = require('sequelize');
 
+// var db = new Sequelize('gamemon', 'root', 'process.env.DB_PASSWORD', {
+//   define: {
+//     charset: 'utf8mb4'
+//   }
+// });
+
 var db = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
   define: {
     charset: 'utf8mb4'
