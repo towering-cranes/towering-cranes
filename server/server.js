03bbf287c1fe2c7d1a4458d212aaf911c979c2e4
gwynndp <gwynndp@gmail.com>
2017-02-11 10:02:04 -0800

    Add basic socket.io chat to index page

--- server/server.js
+++ server/server.js
@@ -13,14 +13,33 @@ var server = app.listen(port, function() {
 app.use(express.static(__dirname + "/../client"));
 app.use(bodyParser.json({limit: '5mb'}));
 
-//Socket.io
+////////////////////Socket.io
+
+var connections = [];
 var io = require('socket.io')(server);
 io.on('connection', function(socket) {
   socket.emit('news', {hello: 'world'});
   socket.on('my other event', function (data) {
     console.log(data);
   });
+
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  socket.on('disconnect', function(data) {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  socket.on('send message', function(data) {
+    console.log(data);
+    io.sockets.emit('new message', {msg: data});
+  });
+
 });
+
+
+//////////////////////////////
 
 // Add a user to db
 app.post('/users', function(req, res) {
