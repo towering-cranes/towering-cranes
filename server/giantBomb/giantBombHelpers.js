3c2368d508a7b1cb008ed32133c9aa5aebfea811
gwynndp <gwynndp@gmail.com>
2017-02-12 18:46:50 -0800

    Style the chat, search & the modal

--- server/giantBomb/giantBombHelpers.js
+++ server/giantBomb/giantBombHelpers.js
@@ -2,11 +2,11 @@ var request = require('request');
 
 // THIS IS FOR LOCAL DEV
 
-//var giantBombApiKey = '';
+var giantBombApiKey = '138240b063cac215ab7ed7413514c1456d3fd46d';
 
 
 // THIS IS FOR THE LIVE SERVER ON HEROKU
-var giantBombApiKey = process.env.GIANTBOMB_API_KEY;
+//var giantBombApiKey = process.env.GIANTBOMB_API_KEY;
 
 exports.searchForGames = function(searchTerm, callback) {
   var options = {
