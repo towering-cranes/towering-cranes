097d33d0130b63fab862ab4007ba1b4a5451003c
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-02-13 14:56:40 -0600

    Fix view collection bug

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -5,7 +5,7 @@ app.controller('OtherCollectionController', function OtherCollectionController($
   $scope.username = $routeParams.username//localStorage.profile;
   //$rootScope.username = localStorage.profile;
   //Store games in corresponding objects
-  if($scope.username = localStorage.name) {
+  if($scope.username === localStorage.name) {
     $location.path('/gamemon');
   };
   $scope.platforms = {};
